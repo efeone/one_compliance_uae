9943997128828e60d3f192db5efef8dca74909a5
mpmuhammad <muhammadmp77@gmail.com>
2026-02-04 12:22:55 +0530

    feat:add doctype

---
+++ one_compliance_uae/one_compliance_uae/doctype/expiry_management/expiry_management.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, efeone and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Expiry Management", {
+// 	refresh(frm) {
+
+// 	},
+// });
